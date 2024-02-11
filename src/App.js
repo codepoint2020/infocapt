67a201d1e0a9c6e02b55f6e05fc417478e271426
JeromeDProgrammer <jeromemorales86@live.com>
2024-02-10 19:54:18 -0800

    Completed User Interface, First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  const handleSubmit = () => {
+    //...
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+
+
+        <div className="form-container">
+          <div><h2>InfoCapture</h2></div>
+          <form onSubmit={handleSubmit}>
+            <input type="text" name="firstname" placeholder='Enter your firstname' required/>
+            <input type="text" name="lastname" placeholder='Lastname' required/>
+            <input type="text" name="city" placeholder='City' required/>
+            <button className='addBtn'>Add Record</button>
+            <button className='updateBtn'>Update Record</button>
+
+          </form>
+        </div>
+
+        <div className="table-container">
+          <h2>DigiPunk World Citizens</h2>
+          <table>
+            <thead> 
+                <tr>
+                  <th>Firstname</th>
+                  <th>Lastname</th>
+                  <th>City</th>
+                  <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                  <td>Jerome</td>
+                  <td>Morales</td>
+                  <td>Davao</td>
+                  <td>
+                      <i class="fa-solid fa-pen-to-square editBtn myBtn"></i>
+                      <i class="fa-solid fa-rectangle-xmark delBtn myBtn"></i>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Mark</td>
+                  <td>Anderson</td>
+                  <td>New York</td>
+                  <td>
+                      <i class="fa-solid fa-pen-to-square editBtn myBtn"></i>
+                      <i class="fa-solid fa-rectangle-xmark delBtn myBtn"></i>
+                  </td>
+                </tr>
+            </tbody>
+          </table>
+
+        </div>
+        
+        <div className="footer">
+          ©  {new Date().getFullYear()} InfoCapture. All rights reserved.
+        </div>
+
+
+      </div>
+    </>
   );
 }
 
